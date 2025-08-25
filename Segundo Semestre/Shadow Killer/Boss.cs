b8e957fa44eac860407a13d47bf1dbba9830358c
raulcommits <raultomaz08@gmail.com>
2025-08-25 01:05:23 -0300

    Projeto de Jogo em trio do Segundo Semestre

---
+++ Segundo Semestre/Shadow Killer/Boss.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Criado por SharpDevelop.
+ * Usuário: Rnote
+ * Data: 15/06/2025
+ * Hora: 21:55
+ * 
+ * Para alterar este modelo use Ferramentas | Opções | Codificação | Editar Cabeçalhos Padrão.
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+namespace Shadow_Killer
+{
+	/// <summary>
+	/// Description of Boss.
+	/// </summary>
+	public class Boss : Personagem
+	{
+		public Boss()
+		{
+			Height = 350;
+			Width = 560;
+			Top = 485;
+			Left = 1120;
+			Load("In_02_Stay.gif");
+			direcao = -1;
+		}
+	}
+}
---
+++ Segundo Semestre/Shadow Killer/Form2.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Shadow_Killer
+{
+    public class Form2 : Form
+    {
+        public Form2()
+        {
+            // Definindo posição da tela de Inicio
+        	StartPosition = FormStartPosition.CenterScreen;
+            Width = 1800;
+            Height = 1250;
+			this.FormBorderStyle = FormBorderStyle.None;
+            this.WindowState = FormWindowState.Maximized;
+            this.TopMost = true;            	
+            
+            // Fundo com GIF animado
+            PictureBox backgroundGif = new PictureBox();
+            backgroundGif.Image = Image.FromFile("BkgTelaInicial.gif");
+            backgroundGif.SizeMode = PictureBoxSizeMode.StretchImage;
+            backgroundGif.Dock = DockStyle.Fill;
+            this.Controls.Add(backgroundGif);
+
+            // Título do Jogo
+            PictureBox title = new PictureBox();
+            title.Image = Image.FromFile("TitleGame01.jpeg");
+            title.Size = new Size(600, 150);
+            title.SizeMode = PictureBoxSizeMode.StretchImage;
+            title.Location = new Point(
+                (this.ClientSize.Width - title.Width) / 2,
+                50
+            );
+            this.Controls.Add(title);
+
+            // Botão "Iniciar"
+            Button iniciar = new Button();
+            iniciar.BackColor = Color.Cornsilk;
+            iniciar.FlatStyle = FlatStyle.Flat;
+            iniciar.FlatAppearance.BorderSize = 3;
+            iniciar.Font = new Font("Arial", 20, FontStyle.Bold);
+            iniciar.Text = "Iniciar Jogo";
+            iniciar.Size = new Size(300, 80);
+            iniciar.Location = new Point(
+                (this.ClientSize.Width - iniciar.Width) / 2,
+                (this.ClientSize.Height - 250)
+            );
+            iniciar.Click += new EventHandler(iniciarClick);
+            this.Controls.Add(iniciar);
+
+            // Para os controles ficarem acima do GIF
+            backgroundGif.SendToBack();
+        }
+		
+        // Método para o aperto do botão e iniciar o Jogo
+        void iniciarClick(object sender, EventArgs e)
+        {
+            MainForm.abrirJogo = true;
+            this.Close();
+        }
+    }
+}
